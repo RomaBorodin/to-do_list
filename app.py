2a5f025cc14a2b26a9bfccceb830f1a7928bfdca
BorodinRoman <borodinroman966@gmail.com>
2025-05-07 22:38:04 +1000

    add routes logic

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 app = Flask(__name__)
@@ -24,3 +23,48 @@ class Task(db.Model):
 
 with app.app_context():
     db.create_all()
+
+
+@app.route('/')
+@app.route('/tasks')
+def get_tasks():
+    tasks = Task.query.all()
+    return render_template('tasks.html', tasks=tasks)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_task():
+    if request.method == 'POST':
+        title = request.form['title']
+        if title.strip():
+            new_task = Task(title=title)
+            db.session.add(new_task)
+            db.session.commit()
+        return redirect(url_for('get_tasks'))
+
+    return render_template('add_task.html')
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_task(id):
+    task = Task.query.get_or_404(id)
+    if request.method == 'POST':
+        new_title = request.form['title']
+        if new_title.strip():
+            task.title = new_title
+            db.session.commit()
+        return redirect(url_for('get_tasks'))
+
+    return render_template('edit_task.html', task=task)
+
+
+@app.route('/delete/<int:id>')
+def delete_task(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('get_tasks'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
